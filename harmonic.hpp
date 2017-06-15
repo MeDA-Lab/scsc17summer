5a7daddb246eac6ab075f56996deda293d09e768
Yuhsiang Tsai <yhmtsai@gmail.com>
2017-06-15 17:17:07 +0800

    add cmath header

--- harmonic.hpp
+++ harmonic.hpp
@@ -7,6 +7,7 @@
 #include <getopt.h>
 #include <tuple>
 #include <vector>
+#include <cmath>
 using namespace std;
 // typedef tuple<int, int, int> int3;
 // typedef tuple<double, double, double> double3;
