4a0eac99636964fb853f4ee4beabd2ef1b2f8627
Yuhsiang Tsai <yhmtsai@gmail.com>
2017-06-18 02:08:34 +0800

    delete the testing output

--- src/core/read_object.cpp
+++ src/core/read_object.cpp
@@ -24,12 +24,11 @@ void readObject(
     cerr<<"Open File "<<filename<<" Error\n";
     exit(1);
   }
-  // Read the vertex
+  // Read vertex
   string tail, head;
   if (fin.peek()=='#'){
     // # nv vertex
     fin>>head>>*ptr_nv>>tail;
-    // cout<<head<<" "<<*ptr_nv<<" "<<tail<<endl;
     while (fin.get()!='\n');
   }
   int nv=*ptr_nv;
@@ -38,7 +37,6 @@ void readObject(
   *C = new double [nv * 3];
   double *VV=*V, *CC=*C;
   // v x y z (R G B)
-  
   for (int i=0; i<nv; i++){
     if (fin.peek()!='v'){
       cerr<<"Read vertex error\n";
@@ -55,16 +53,13 @@ void readObject(
       CC[nv+i]     = -1;
       CC[2*nv+i]  = -1;
     }
-    // cout<<head<<" "<<V[i]<<" "<<V[nv+i]<<" "<<V[nv+nv+i];
-    // cout<<" "<<C[i]<<" "<<C[nv+i]<<" "<<C[nv+nv+i]<<endl;
     while (fin.get()!='\n');
   }
 
-  // Read the face
+  // Read faces
   if (fin.peek()=='#'){
     // # nf faces
     fin>>head>>*ptr_nf>>tail;
-    // cout<<head<<" "<<*ptr_nf<<" "<<tail<<endl;
     while (fin.get()!='\n');
   }
   int nf = *ptr_nf;
@@ -72,13 +67,13 @@ void readObject(
   *F = new int [ nf * 3 ];
   int *FF=*F;
   for (int i=0; i<nf; i++){
+    // f p1 p2 p3
     if (fin.peek()!='f'){
       cerr<<"Read Face error\n";
       printf("%c",fin.peek());
       exit(1);
     }
     fin>>head>>FF[i]>>FF[nf+i]>>FF[2*nf+i];
-    // cout<<head<<" "<<F[i]<<" "<<F[nv+i]<<" "<<F[nv+nv+i]<<endl;
     while (fin.get()!='\n');
   }
   while (fin.get()=='\n');
