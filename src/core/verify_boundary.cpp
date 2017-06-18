3a01060c7422ac55f494b8154cd58dd024ebbab9
Yuhsiang Tsai <yhmtsai@gmail.com>
2017-06-18 17:30:46 +0800

    delete tesing output

--- src/core/verify_boundary.cpp
+++ src/core/verify_boundary.cpp
@@ -31,7 +31,6 @@ void verifyBoundary(
     int *ptr_nb,
     int **idx_b
 ) {
-  cout<<"Start\n";
   int *Gvv = new int [nv*nv];
   for (int i=0; i<nv*nv; i++){
     Gvv[i] = 0;
@@ -41,12 +40,10 @@ void verifyBoundary(
     p[0]=F[i];
     p[1]=F[nf+i];
     p[2]=F[2*nf+i];
-    cout<<p[0]<<" "<<p[1]<<" "<<p[2]<<endl;
     Gvv[(p[1]-1)*nv+(p[0]-1)]++; // p0->p1
     Gvv[(p[2]-1)*nv+(p[1]-1)]++; // p1->p2
     Gvv[(p[0]-1)*nv+(p[2]-1)]++; // p2->p0
   }
-  cout<<"Build End\n";
   int nb=0, bd_vertex=nv+1, bd_ind=nv+1;
   int *Bi= new int [nb], *Bj = new int [nb];
   for (int j=0; j<nv; j++){
@@ -63,7 +60,6 @@ void verifyBoundary(
       }
     }
   }
-  cout<<"Find Po\n";
   vector<int> VB;
   for (int i=0; i<nb; i++){
     VB.push_back(bd_vertex);
@@ -73,14 +69,10 @@ void verifyBoundary(
       break;
     }
   }
-  cout<<VB.size()<<"Find Boundary"<<nb<<"\n";
   *ptr_nb = VB.size();
-  cout<<"b"<<*ptr_nb<<"\n";
   *idx_b = new int [*ptr_nb];
   for (int i=0; i<*ptr_nb; i++){
     (*idx_b)[i]=VB[i];
-    cout <<VB[i]<<" ";
   }
-  cout<<"\n";
   return;
 }
