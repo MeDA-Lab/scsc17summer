d4b78bcf2e440aec390d201b88b1cc3ea8a9647b
Mu Yang <emfomy@gmail.com>
2017-06-24 12:45:15 +0800

    Update sparse main function.

--- src/main_sparse.cpp
+++ src/main_sparse.cpp
@@ -1,5 +1,5 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-/// @file    main_sparse.hpp
+/// @file    main.hpp
 /// @brief   The main function. (sparse version)
 ///
 /// @author  Mu Yang <<emfomy@gmail.com>>
@@ -18,51 +18,60 @@ int main( int argc, char** argv ) {
 
   const char *input  = "input.obj";
   const char *output = "output.obj";
-  Method method = Method::KIRCHHOFF;
+  Method method  = Method::KIRCHHOFF;
 
-  int nv, nf, nb, *F = nullptr, *idx_b = nullptr;
-  int Lii_nnz = 0, Lib_nnz = 0, *Lii_row = nullptr, *Lii_col = nullptr, *Lib_row = nullptr, *Lib_col = nullptr;
-  double *Lii_val = nullptr, *Lib_val = nullptr;
-  double *V = nullptr, *C = nullptr, *U;
+  int nv, nf, nb, Lii_nnz = 0, Lib_nnz = 0, *F = nullptr, *idx_b,
+      *Lii_row = nullptr, *Lii_col = nullptr, *Lib_row = nullptr, *Lib_col = nullptr;
+  double timer, *V = nullptr, *C = nullptr, *Lii_val = nullptr, *Lib_val = nullptr, *U;
+
 
   // Read arguments
-  cout << "Reading arguments ..." << endl;
   readArgs(argc, argv, input, output, method);
 
   // Read object
-  cout << "Reading objects ..." << endl;
   readObject(input, &nv, &nf, &V, &C, &F);
 
-  // Verify boundary
-  cout << "Verifying boundary ..." << endl;
-  idx_b = new int[nv];
-  verifyBoundarySparse(nv, nf, F, &nb, idx_b);
+  cout << endl;
 
-  // Reorder vertex
-  cout << "Reordering vertex ..." << endl;
-  reorderVertex(nv, nb, nf, idx_b, V, C, F);
+  // Verify boundary
+  idx_b = new int[nv];
+  cout << "Verifying boundary ....................." << flush;
+  tic(&timer);
+  verifyBoundarySparse(nv, nf, F, &nb, idx_b); cout << " Done.  ";
+  toc(&timer);
+
+  // Reorder vertices
+  cout << "Reordering vertices ...................." << flush;
+  tic(&timer);
+  reorderVertex(nv, nb, nf, idx_b, V, C, F); cout << " Done.  ";
+  toc(&timer);
 
   // Construct Laplacian
-  cout << "Constructing Laplacian ..." << endl;
-  constructLaplacianSparse(method, nv, nb, nf, V, F,
-                           &Lii_val, &Lii_row, &Lii_col, &Lii_nnz,
-                           &Lib_val, &Lib_row, &Lib_col, &Lib_nnz);
+  cout << "Constructing Laplacian ................." << flush;
+  tic(&timer);
+  constructLaplacianSparse(method, nv, nb, nf, V, F, &Lii_val, &Lii_row, &Lii_col, &Lii_nnz,
+                                                     &Lib_val, &Lib_row, &Lib_col, &Lib_nnz); cout << " Done.  ";
+  toc(&timer);
 
   // Map boundary
-  cout << "Maping boundary ..." << endl;
   U = new double[2 * nv];
-  mapBoundary(nv, nb, V, U);
-
+  cout << "Mapping Boundary ......................." << flush;
+  tic(&timer);
+  mapBoundary(nv, nb, V, U); cout << " Done.  ";
+  toc(&timer);
 
   // Solve harmonic
-  cout << "Solving harmonic ..." << endl;
-  solveHarmonicSparse(nv, nb, Lii_val, Lii_row, Lii_col, Lib_val, Lib_row, Lib_col, U);
+  cout << "Solving Harmonic ......................." << flush;
+  tic(&timer);
+  solveHarmonicSparse(nv, nb, Lii_val, Lii_row, Lii_col, Lib_val, Lib_row, Lib_col, U); cout << " Done.  ";
+  toc(&timer);
+
+  cout << endl;
+
   // Write object
-  cout << "Writing object ..." << endl;
   writeObject(output, nv, nf, U, C, F);
 
   // Free memory
-  cout << "Done." << endl;
   delete[] V;
   delete[] C;
   delete[] F;
