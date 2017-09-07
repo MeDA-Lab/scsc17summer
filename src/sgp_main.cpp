c32d37180fa52e604b3b372ee95bffdfa2a14e54
William Liao <r05246012@ntu.edu.tw>
2017-09-08 00:18:57 +0800

    test

--- src/sgp_main.cpp
+++ src/sgp_main.cpp
@@ -60,7 +60,7 @@ int main( int argc, char** argv ){
     double mu0 = 1.5, mu;
     double *x;
     x = new double[n];
-    char flag = 'H';
+    char flag = 'D';
 
     switch (flag){
     	case 'H':
