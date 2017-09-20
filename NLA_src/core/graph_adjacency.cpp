d095f7e2e00d19fedd43253c14781087909a78a2
William Liao <r05246012@ntu.edu.tw>
2017-09-20 23:36:31 +0800

    test

--- NLA_src/core/graph_adjacency.cpp
+++ NLA_src/core/graph_adjacency.cpp
@@ -44,83 +44,71 @@ int GraphAdjacency(int *E, int E_size,
 	*n   = max(E[pos1] , E[pos2+E_size])+1;
 	//cout << "n = " << *n << endl;
 
-	switch(flag){
-		case 'S':
-		{
-			int  *job, *csrRowInd, *csrColInd, *cooRowInd, *cooColInd;
-			double  *csrVal, *cooVal;
+	if( strcmp(flag , 'S') )
+	{
+		int  *job, *csrRowInd, *csrColInd, *cooRowInd, *cooColInd;
+		double  *csrVal, *cooVal;
 
-			cooRowInd = new int[E_size];
-			cooColInd = new int[E_size];
-			cooVal    = new double[E_size];
+		cooRowInd = new int[E_size];
+		cooColInd = new int[E_size];
+		cooVal    = new double[E_size];
 
-			job = new int[6];
-			*nnz = E_size;
-			copy(E , E+E_size , cooRowInd);
-			copy(E+E_size, E+2*E_size, cooColInd);
-			copy(v1.begin(), v1.end(), cooVal);
+		job = new int[6];
+		*nnz = E_size;
+		copy(E , E+E_size , cooRowInd);
+		copy(E+E_size, E+2*E_size, cooColInd);
+		copy(v1.begin(), v1.end(), cooVal);
 
-			// A+trans(A)
-			csrVal    = new double[*nnz];
-			csrRowInd = new int[*n+1];
-			csrColInd = new int[*nnz];
+		// A+trans(A)
+		csrVal    = new double[*nnz];
+		csrRowInd = new int[*n+1];
+		csrColInd = new int[*nnz];
 
-			job[0] = 2;
-		  	job[1] = 1;
-		  	job[2] = 0;
-		  	job[4] = (*n)*(*n);
-		  	job[5] = 0;
-			mkl_dcsrcoo(job, n, csrVal, csrColInd, csrRowInd, nnz, cooVal, cooRowInd, cooColInd, &info);
-			delete cooVal;
-			delete cooRowInd;
-			delete cooColInd;
-			cooRowInd = new int[*n+1];
-			int nzmax = (*n)*(*n);
-			mkl_dcsradd(&trans, &request, &sort, n, n, csrVal, csrColInd, csrRowInd, &beta, csrVal, csrColInd, csrRowInd, cooVal, cooColInd, cooRowInd, &nzmax, &info);
-			assert( info == 0 );
-			*nnz = cooRowInd[*n]-1;
-			request = 2;
-			cooVal    = new double[*nnz];
-			cooColInd = new int[*nnz];
-			mkl_dcsradd(&trans, &request, &sort, n, n, csrVal, csrColInd, csrRowInd, &beta, csrVal, csrColInd, csrRowInd, cooVal, cooColInd, cooRowInd, &nzmax, &info);
-			assert( info == 0 );
+		job[0] = 2;
+	  	job[1] = 1;
+	  	job[2] = 0;
+	  	job[4] = (*n)*(*n);
+	  	job[5] = 0;
+		mkl_dcsrcoo(job, n, csrVal, csrColInd, csrRowInd, nnz, cooVal, cooRowInd, cooColInd, &info);
+		delete cooVal;
+		delete cooRowInd;
+		delete cooColInd;
+		cooRowInd = new int[*n+1];
+		int nzmax = (*n)*(*n);
+		mkl_dcsradd(&trans, &request, &sort, n, n, csrVal, csrColInd, csrRowInd, &beta, csrVal, csrColInd, csrRowInd, cooVal, cooColInd, cooRowInd, &nzmax, &info);
+		assert( info == 0 );
+		*nnz = cooRowInd[*n]-1;
+		request = 2;
+		cooVal    = new double[*nnz];
+		cooColInd = new int[*nnz];
+		mkl_dcsradd(&trans, &request, &sort, n, n, csrVal, csrColInd, csrRowInd, &beta, csrVal, csrColInd, csrRowInd, cooVal, cooColInd, cooRowInd, &nzmax, &info);
+		assert( info == 0 );
 
-			job[0] = 0;
-			job[4] = *nnz;
-			job[5] = 3;
-			*cooValA    = new double[*nnz];
-			*cooRowIndA = new int[*nnz];
-			*cooColIndA = new int[*nnz];
-			mkl_dcsrcoo(job, n, cooVal, cooColInd, cooRowInd, nnz, *cooValA, *cooRowIndA, *cooColIndA, &info);
-			//cout << "info = " << info << endl;
-			assert( info == 0 );
-
-			break;
-		}
-		case 'W':
-		{
-			*nnz = E_size;
-			*cooValA    = new double[*nnz];
-			*cooRowIndA = new int[*nnz];
-			*cooColIndA = new int[*nnz];
-			copy(E , E+E_size , *cooRowIndA);
-			copy(E+E_size, E+2*E_size, *cooColIndA);
-			copy(E+2*E_size, E+3*E_size, *cooValA);
-
-			break;
-		}
-		case 'D':
-		{
-			*nnz = E_size;
-			*cooValA    = new double[*nnz];
-			*cooRowIndA = new int[*nnz];
-			*cooColIndA = new int[*nnz];
-			copy(E , E+E_size , *cooRowIndA);
-			copy(E+E_size, E+2*E_size, *cooColIndA);
-			copy(v1.begin(), v1.end(), *cooValA);
-
-			break;
-		}
+		job[0] = 0;
+		job[4] = *nnz;
+		job[5] = 3;
+		*cooValA    = new double[*nnz];
+		*cooRowIndA = new int[*nnz];
+		*cooColIndA = new int[*nnz];
+		mkl_dcsrcoo(job, n, cooVal, cooColInd, cooRowInd, nnz, *cooValA, *cooRowIndA, *cooColIndA, &info);
+		//cout << "info = " << info << endl;
+		assert( info == 0 );
+	}else if( strcmp(flag , 'W') ){
+		*nnz = E_size;
+		*cooValA    = new double[*nnz];
+		*cooRowIndA = new int[*nnz];
+		*cooColIndA = new int[*nnz];
+		copy(E , E+E_size , *cooRowIndA);
+		copy(E+E_size, E+2*E_size, *cooColIndA);
+		copy(E+2*E_size, E+3*E_size, *cooValA);
+	}else if( strcmp(flag , 'D')){
+		*nnz = E_size;
+		*cooValA    = new double[*nnz];
+		*cooRowIndA = new int[*nnz];
+		*cooColIndA = new int[*nnz];
+		copy(E , E+E_size , *cooRowIndA);
+		copy(E+E_size, E+2*E_size, *cooColIndA);
+		copy(v1.begin(), v1.end(), *cooValA);
 	}
 
 	stat = cusparseCreate(&handle);
